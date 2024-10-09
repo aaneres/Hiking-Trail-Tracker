c6e64e91a15b34d1a58ee1007f0a0f6fa590b48a
aaneres <159407812+aaneres@users.noreply.github.com>
2024-10-09 11:34:25 -0700

    Added documentation for TrailLog class

---
+++ src/main/ui/TrailLog.java
@@ -0,0 +1,57 @@
+package ui;
+
+import model.Trail;
+
+import java.util.Scanner;
+
+
+// Collection of Trails
+public class TrailLog {
+    private Trail currentTrail;
+    private Scanner input;
+
+    // EFFECTS: runs the trail log
+    public TrailLog() {
+        runTrailLog();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runTrailLog() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user command
+    private void processCommand(String command) {
+        // stub
+    }
+
+    // EFFECTS: displays menu of options to user
+    private void displayMenu() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a Trail to TrailLog
+    private void addTrail() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes a Trail from TrailLog
+    private void removeTrail() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: changes a chosen trail's completion status
+    private void changeTrailCompletion() {
+        // stub
+    }
+
+    // EFFECTS: displays all trails in TrailLog
+    private void viewTrails() {
+        // stub
+    }
+}
